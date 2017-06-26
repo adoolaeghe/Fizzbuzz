2b785771f61b1d38b6e3620ea3ffbc240b4beddc
antoinedoolaeghe <antoinedoolaeghe@gmail.com>
2017-06-26 12:30:08 +0200

    Added multiple of 3 AND 5 and corrected previous tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,5 @@
 def fizzbuzz (number)
-case 
-when number % 3 == 0
-   return 'fizz'
-when number % 5 == 0
-   return 'buzz'
- end
+  return "fizz" if number % 3 == 0 && number % 5 != 0
+  return "buzz" if number % 5 == 0 && number % 3 != 0
+  return "fizzbuzz" if number % 3 == 0 && number % 5 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,21 @@
 require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed a multiple of 3 ' do
+  it 'returns "fizz" when passed a multiple of 3 AND NOT 5 ' do
     (1..100).each do |n|
-    expect(fizzbuzz(n)).to eq 'fizz' if n % 3 == 0
-  end
-end
-end
- describe 'fizzbuzz' do
-    it 'returns "buzz" when passed a multiple of 5 ' do
-      (1..100).each do |n|
-        expect(fizzbuzz(n)).to eq 'buzz' if n % 5 == 0
+    expect(fizzbuzz(n)).to eq 'fizz' if n % 3 == 0 && n % 5 != 0
     end
   end
+
+    it 'returns "buzz" when passed a multiple of 5 AND NOT 3' do
+      (1..100).each do |n|
+        expect(fizzbuzz(n)).to eq 'buzz' if n % 5 == 0 && n % 3 != 0
+    end
+  end
+
+   it 'returns "buzz" when passed a multiple of 3 and  5 ' do
+     (1..100).each do |n|
+       expect(fizzbuzz(n)).to eq 'fizzbuzz' if n % 3 == 0 && n % 5 == 0
+   end
+ end
 end
